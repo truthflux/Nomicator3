195efaa29095dc8a1028905e468a3ed85683ae1b
michaelcarratt <truthflux@gmail.com>
2014-09-21 22:31:45 -0400

    initial git repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nomicator3::Application.config.secret_key_base = '75266bdb26ef1da0a2e1ed06eaf2ddd2345fe56f0b5e07d6ad861bd8a2a0df9b8ab3246ad3081d96f46d6b2be6c9741851cdd3d7c1303840647e80fb98f618c4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Nomicator3::Application.config.session_store :cookie_store, key: '_Nomicator3_session'
