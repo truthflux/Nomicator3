08219dddd1c0cb72d45c311768654bb9fc1e3749
michaelcarratt <truthflux@gmail.com>
2014-09-23 18:15:38 -0400

    Add signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Nomicator3::Application.routes.draw do
+  get "users/new"
+
   root 'static_pages#home'
   match '/about', to: 'static_pages#about', via: 'get'
+
+  match '/signup', to: 'users#new', via: 'get'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
