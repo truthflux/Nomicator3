93cc7696e39f6529e87a4627f2373c727edf5cd5
michaelcarratt <truthflux@gmail.com>
2014-09-24 17:00:21 -0400

    Test branch - attribute accessors no longer work in Rails 4.0

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,61 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :correct_user, only: [:edit, :update]
+  before_filter :admin_user, only: [:destroy]
+
+  def new
+    @user = User.new
+  end
+
   def show
     @user = User.find(params[:id])
   end
 
-  def new
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to The Nomicator!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
+
+  private
+
+  #before filters
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Nomicator3::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root 'static_pages#home'
 
   match '/about', to: 'static_pages#about', via: 'get'
 
   match '/signup', to: 'users#new', via: 'get'
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'get'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
